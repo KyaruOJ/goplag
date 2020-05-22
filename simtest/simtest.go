3610122ed7fd640fb8ff1008e1a8e0695ea70b6b
kotoyuuko <kokoro@core.moe>
2020-05-22 10:24:34 +0800

    sync

--- simtest/simtest.go
+++ simtest/simtest.go
@@ -5,6 +5,7 @@ import (
 	"goplag/lexer"
 	"goplag/source"
 	"goplag/winnowing"
+	"sort"
 	"strconv"
 )
 
@@ -23,32 +24,50 @@ func Simtest(srcA, srcB *source.Source, raw bool) (string, error) {
 
 	fingerprintsA := winnowing.Winnowing(tokensA)
 	fingerprintsB := winnowing.Winnowing(tokensB)
-	fingerprintsSame := make(map[int]string)
+	interval := make([]int, 0)
 
-	same := 0
 	for idx, fpA := range fingerprintsA {
 		for _, fpB := range fingerprintsB {
 			if fpA == fpB {
-				fingerprintsSame[idx] = fpA
+				interval = append(interval, idx)
 				break
 			}
 		}
 	}
 
-	ans := int(len(fingerprintsSame) * 100 / len(fingerprintsA))
+	sort.Ints(interval)
+	N := winnowing.N
+	merged := make([][]int, 0)
+	merged = append(merged, []int{interval[0], interval[0] + N})
+	for _, startAt := range interval {
+		m := merged[len(merged)-1]
+
+		if startAt > m[1] {
+			merged = append(merged, []int{startAt, startAt + N})
+			continue
+		}
+
+		if startAt+N > m[1] {
+			m[1] = startAt + N
+		}
+	}
+
+	length := 0
+	for _, item := range merged {
+		length += item[1] - item[0]
+	}
+
+	ans := int(length * 100 / len(tokensA))
 
 	if raw {
 		res := ""
 
 		res += "similarity: "
 		res += strconv.Itoa(ans) + "% "
-		res += "(" + strconv.Itoa(same) + " / " + strconv.Itoa(len(fingerprintsA)) + ")" + "\n"
+		res += "(" + strconv.Itoa(length) + " / " + strconv.Itoa(len(tokensA)) + ")" + "\n"
 
 		res += "tokens: "
-		res += strconv.Itoa(len(tokensA)) + " - " + strconv.Itoa(len(tokensB)) + "\n"
-
-		res += "fingerprints: "
-		res += strconv.Itoa(len(fingerprintsA)) + " - " + strconv.Itoa(len(fingerprintsB))
+		res += strconv.Itoa(len(tokensA)) + " - " + strconv.Itoa(len(tokensB))
 
 		return res, nil
 	}
